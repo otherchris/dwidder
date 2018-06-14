e23d5706e4eb27e4e137aba181a6138dbdab65c1
Chris Caragianis <chriscaragianis@gmail.com>
2018-06-13 23:36:47 -0400

    client connects to websocket

--- client/src/hoc/socketConnect.js
+++ client/src/hoc/socketConnect.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
-const fakeData = { hello: "world" };
+import { Socket } from 'phoenix';
 
 const socketConnect = (Component) => {
   return class extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        socket: new Socket('ws://localhost:4000/socket', {})
+      }
     }
+
+    componentDidMount() {
+      this.state.socket.connect();
+    }
+
     render() {
-      return <Component data={fakeData} />
+      return <Component data="data" />
     }
   }
 };
