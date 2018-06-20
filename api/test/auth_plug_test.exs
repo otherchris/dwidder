05e07985f88ad51c0c37b750b7dae06200a1edfb
Chris Caragianis <chriscaragianis@gmail.com>
2018-06-20 15:35:43 -0500

    ad auth plug

---
+++ api/test/auth_plug_test.exs
@@ -0,0 +1,39 @@
+defmodule Api.AuthenticationTest do
+  use ApiWeb.ConnCase
+
+  alias ApiWeb.{Authentication, User, Session}
+  alias Api.Repo
+
+  @opts Authentication.init([])
+
+  def put_auth_token_in_header(conn, token) do
+    conn
+    |> put_req_header("authorization", "Token token=\"#{token}\"")
+  end
+
+  test "finds the user by token", %{conn: conn} do
+    user = Repo.insert!(%User{email: "test@em.ail"})
+    session = Repo.insert!(%Session{token: "123", user_id: user.id})
+
+    conn = conn
+    |> put_auth_token_in_header(session.token)
+    |> Authentication.call(@opts)
+
+    assert conn.assigns.current_user
+  end
+
+  test "invalid token", %{conn: conn} do
+    conn = conn
+    |> put_auth_token_in_header("foo")
+    |> Authentication.call(@opts)
+
+    assert conn.status == 401
+    assert conn.halted
+  end
+
+  test "no token", %{conn: conn} do
+    conn = Authentication.call(conn, @opts)
+    assert conn.status == 401
+    assert conn.halted
+  end
+end
